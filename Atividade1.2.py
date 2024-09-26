a704f59146dd089c4e72c44e4d0bf4423fc6234a
Johnie <johnie.price@pm.me>
2024-09-26 11:56:18 -0300

    Atividade1.2 Consumo de combustivel

--- Atividade1.2.py
+++ Atividade1.2.py
@@ -2,7 +2,7 @@
 # Desenvolver um fluxograma e um programa em Python para determinar o
 # consumo médio de um automóvel sendo fornecida a distância total percorrida
 # pelo automóvel e o total de combustível gasto.
-# CONSUMO=DITANCIA_PERCORRIDA/COMBUSTIVEL_GASTO
+# CONSUMO=DITANCIA_PERCORRIDA/COMBUSTIVEL_GASTO#
 
 distancia_percorrida = float(input("Digite a distancia percorrida: "))
 combustivel_gasto = float(input("Digite a quantidade de combustivel gasto: "))
