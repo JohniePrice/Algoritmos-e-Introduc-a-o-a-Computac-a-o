d95e8a44dd7c2ecc8f4d7b05aebc851ba1ea068c
JohniePrice <Johnie.price@protonmail.com>
2024-09-27 11:20:05 -0300

    Atividade1.6 valor depositado

---
+++ Atividade1.8.py
@@ -0,0 +1,6 @@
+# Desenvolver um fluxograma e um programa em Python que receba um valor que
+# foi depositado e exiba o valor com rendimento após um mês. Considere fixo o
+# juro da poupança em 0,70%.
+
+deposito = float(input("Digite o valor a ser depositado: "))
+print(" Rendimento após um mes: "+ deposito * 1.7)
