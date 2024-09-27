64358b6b895a6aa09c60da8c80644f3cb457784a
JohniePrice <Johnie.price@protonmail.com>
2024-09-27 12:30:39 -0300

    Atividade1.13 Categoria de nadadores

--- Atividade1.13.py
+++ Atividade1.13.py
@@ -6,3 +6,16 @@
 # • Juvenil B = 14 - 17 anos
 # • Sênior = 18 - 25 anos
 
+idade = int(input("Digite sua idade: "))
+
+if idade >= 5 and idade <= 7:
+    print("Infantil A")
+elif idade >= 8 and idade <= 10:
+    print("Infantil B")
+elif idade >= 11 and idade <= 13:
+    print("Juvenil A")
+elif idade >= 14 and idade <= 17:
+    print("Juvenil B")
+elif idade >= 18 and idade <= 25:
+    print("Senior")
+
